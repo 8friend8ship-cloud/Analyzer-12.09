42e7f70683854831e5a8bf3123385e98a25a61ee
8friend8ship-cloud <homedesigntaedi@gmail.com>
2026-08-20 15:48:55 +0900

    feat: activate free Content OS backdata policy

--- index.tsx
+++ index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import ErrorBoundary from './components/ErrorBoundary';
+import { installFreeOperationPolicy } from './services/freeOperationPolicy';
+
+installFreeOperationPolicy();
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
